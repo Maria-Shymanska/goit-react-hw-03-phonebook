2ad60bf5e71d1c12ff5c4eb9d29fbebef9fa52e2
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-05-07 22:00:32 +0200

    add new files

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Label, Button } from './ContactForm.styled';
+
+class ContactForm extends Component {
+  // 1) These states are not needed in the APP until the time of SUBMIT the entire form/temporary data
+  state = {
+    name: '',
+    number: '',
+  };
+
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // <ContactForm onSubmit={this.addContact} => Retrieving data in the APP;
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Label>
+          Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+        <Label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            placeholder="Enter phone number"
+            value={this.state.number}
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Form>
+    );
+  }
+}
+
+export default ContactForm;
