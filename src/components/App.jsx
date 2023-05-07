d54d3191ad47753638992ecd133b6c9ebf00e604
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-05-07 22:09:41 +0200

    add DidMount and didUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,20 @@ class App extends React.Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = ({ name, number }) => {
     const contact = {
       name,
